0e2f6e1992899320273a66ffe22652b5b0f3542a
rbatist10 <39437144+rbatist10@users.noreply.github.com>
2020-03-06 14:23:20 -0300

    create news.php

---
+++ news.php
@@ -0,0 +1,37 @@
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+	<style>
+		* {
+			box-sizing: border-box;
+			font-family: 'Montserrat';
+		}
+		body {
+			color: #9D61B0;
+		}
+		h1 {
+			text-align: center;
+			padding: 10px;
+		}
+		.btn-purple.disabled, .btn-purple:disabled {
+			color: #fff;
+			background-color: #ed7f9f;
+			border-color: #ed7f9f;
+		}
+		.btn-purple {
+			color: #fff;
+			background-color: #ed7f9f;
+			border-color: #ed7f9f;
+			padding: 3px 19px;
+			margin-left: 5px;
+		}
+	</style>
+	
+	<section>
+		<form action="https://www.rubyrose.com.br/newsletter/list.php" method="POST">
+			<input type="text" class="form-group form-sm">
+			<input type="submit" value="cadastrar" class="btn btn-md btn-purple">
+		</form>
+	</section>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
