bf54deb6d2b43fbebc905c06188fd571181e1a83
rbatist10 <39437144+rbatist10@users.noreply.github.com>
2020-03-12 09:52:13 -0300

    Update news.php

--- news.php
+++ news.php
@@ -2,7 +2,7 @@
 	<style>
 		* {
 			box-sizing: border-box;
-			font-family: 'Montserrat';
+			
 		}
 		body {
 			color: #9D61B0;
@@ -33,7 +33,3 @@
 			<input type="submit" value="cadastrar" class="btn btn-md btn-purple">
 		</form>
 	</section>
-
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
