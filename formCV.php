bfeb95e825337a6eb15671861fff90ec3790f1dd
rbatist10 <39437144+rbatist10@users.noreply.github.com>
2020-03-13 16:06:02 -0300

    Update formCV.php

--- formCV.php
+++ formCV.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-<html lang='pt-br'>
-<header>
-	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
-</header>
 <style>
 	* {
 		
@@ -11,10 +6,6 @@
 /*# sourceMappingURL=bootstrap.min.css.map */
 	body {
 		color: #9D61B0;
-	}
-	h1 {
-		text-align: center;
-		padding: 10px;
 	}
 	.btn-purple.disabled, .btn-purple:disabled {
 		color: #fff;
@@ -87,9 +78,4 @@
 			</div>
 		</div>
 	</section>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>	
-</script>
 </body>
-</html>
