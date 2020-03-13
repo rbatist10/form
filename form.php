3f884f21545c337796d3ed33ed6a51bf7dbd895b
rbatist10 <39437144+rbatist10@users.noreply.github.com>
2020-03-13 16:14:50 -0300

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,6 @@
-  
-<!DOCTYPE html>
-<html lang='pt-br'>
-<header>
-	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
-</header>
 <style>
 	* {
 		box-sizing: border-box;
-		font-family: 'monteserrant';
 	}
 	body {
 		color: #9D61B0;
@@ -185,10 +178,6 @@ background-color: #fff;
 			</div>
 		</section>
 
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
 		<script>
 			function exibeForm(){
 				var div = document.getElementById("formSejaParceiro");
@@ -205,4 +194,3 @@ background-color: #fff;
 			}
 		</script>
 	</body>
-</html>
